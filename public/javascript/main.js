16e0858e3a54599fbd60a8b629b9c38419f66a4e
unknown <yuanyu.tseng@gmail.com>
2014-07-06 12:38:24 +0800

    modified index.html, main.js output as response name

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,25 +1,13 @@
 (function($){
-    var response ={}; 
- 
-    var start=function(){
-        var content=$('#content');
-        var posts=response.posts; //array
-        var html ='';
-        posts.forEach(function(post){
-            html+= '<div class="alert-info">';
-            html+= post.subject;
-            html+= '</div>';
-        });
-        content.html(html);
-    };
     $.ajax({
      dataType: 'json',
      header: {'Content-Type': 'application/json'},
      crossDomain: true,
      url: 'http://booklog.io/1/post',
-     success: function(data, textStatus, jqXHR){
-                response = data;
-                start();
+     success: function(response){
+                  $('#postTemplate')
+                        .tmpl(response.posts)
+                        .appendTo('#content');
         }
     });
 
